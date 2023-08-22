8752609b76dcca4355b1acf8f536f251e15775e8
Sophia <sherman205@gmail.com>
2023-08-21 21:01:06 -0400

    Add SSE logic to consume live test scores from url, establish redis DB, and start /GET students endpoint

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
 """
-Flask routes
+Flask routes for live test score retrieval
 """
-from flask import Flask
+import json
+import sseclient
+from flask import Flask, Response, jsonify
+from db import RedisDB
 
 app = Flask(__name__)
+app.scores_db = RedisDB(db=1)
+app.exams_db = RedisDB(db=2)
 
 
-@app.route('/')
+SCORES_SERVER_URL = "http://live-test-scores.herokuapp.com/scores"
+
+
+def connect_to_server():
+    client = sseclient.SSEClient(SCORES_SERVER_URL)
+
+    for event in client:
+        if event.event == 'score':
+            row = json.loads(event.data)
+            app.scores_db.save_event(row.get('studentId'), row.get('exam'), row.get('score'))
+            app.exams_db.save_event(row.get('exam'), row.get('studentId'), row.get('score'))
+            yield f"data: {row}\n\n"
+
+
+@app.route('/', methods=['GET'])
 def live_test_scores_home():
     """Index route on app load."""
-    return 'Hello world'
+    return Response(connect_to_server(), content_type='text/event-stream')
 
 
-@app.route('/students')
+@app.route('/students', methods=['GET'])
 def get_students():
     """Lists all users that have received at least one test score."""
-    pass
+    students = app.scores_db.get_keys()
+    students = [s.decode() for s in students]
+    return jsonify(students)
 
 
-@app.route('/students/<id>')
+@app.route('/students/<id>', methods=['GET'])
 def get_student_id():
     """
     Lists the test results for the specified student, and provides the student's average score across all exams.
@@ -26,13 +47,13 @@ def get_student_id():
     pass
 
 
-@app.route('/exams')
+@app.route('/exams', methods=['GET'])
 def get_exams():
     """Lists all the exams that have been recorded."""
     pass
 
 
-@app.route('/exams/<id>')
+@app.route('/exams/<id>', methods=['GET'])
 def get_exam_id():
     """Lists all the results for the specified exam, and provides the average score across all students."""
     pass
---
+++ db.py
@@ -0,0 +1,15 @@
+import redis
+
+
+class RedisDB:
+    def __init__(self, host='localhost', port=6379, db=0):
+        self.redis_client = redis.StrictRedis(host=host, port=port, db=db)
+
+    def get_event(self, key):
+        return self.redis_client.hgetall(key)
+
+    def save_event(self, name, key, value):
+        self.redis_client.hset(name, key, value)
+
+    def get_keys(self):
+        return self.redis_client.keys()
