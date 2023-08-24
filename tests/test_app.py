b7af6e5c65fb6aea1246dd5510b3d31568568b7b
Sophia <sherman205@gmail.com>
2023-08-24 17:06:15 -0400

    Add more testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,71 @@
 """
 Tests for app.py
 """
-from app import app
+from app import app, average_score, celery, process_test_scores_data
 from unittest import TestCase
 from db import RedisDB
+
+import json
+import unittest
+from unittest.mock import patch, call, Mock
+
+
+@patch('app.sseclient.SSEClient')
+@patch('app.app.students_db.save_event')
+@patch('app.app.exams_db.save_event')
+class TestProcessTestScoresData(unittest.TestCase):
+
+	def test_process_test_scores_data(self, mock_save_exam, mock_save_student, mock_sseclient):
+		"""
+		Tests simulating processing data from an SSE server and adding the correct
+		events to each mock database.
+		"""
+		# Mock SSE event data
+		event_data = [
+			Mock(event='score', data=json.dumps({
+				'studentId': 'studentId12345',
+				'exam': '150',
+				'score': '0.90'
+			})),
+			Mock(event='score', data=json.dumps({
+				'studentId': 'studentId56789',
+				'exam': '180',
+				'score': '0.70'
+			}))
+		]
+
+		# Configure mock SSEClient behavior
+		mock_sseclient.return_value.__iter__.return_value = iter(event_data)
+
+		process_test_scores_data()
+
+		self.assertEqual(mock_save_student.call_count, 2)
+		self.assertEqual(mock_save_exam.call_count, 2)
+
+		mock_save_student.assert_has_calls(
+			calls=[call('studentId12345', '150', '0.90'), call('studentId56789', '180', '0.70')]
+		)
+		mock_save_exam.assert_has_calls(
+			calls=[call('150', 'studentId12345', '0.90'), call('180', 'studentId56789', '0.70')]
+		)
+
+	def test_process_test_scores_incorrect_event(self, mock_save_exam, mock_save_student, mock_sseclient):
+		"""Tests that no action is taken if the event type is not "score"."""
+		# Mock SSE event data
+		event_data = [
+			Mock(event='test', data=json.dumps({
+				'studentId': 'studentId12345',
+				'name': 'Jill'
+			}))
+		]
+
+		# Configure mock SSEClient behavior
+		mock_sseclient.return_value.__iter__.return_value = iter(event_data)
+
+		process_test_scores_data()
+
+		mock_save_exam.assert_not_called()
+		mock_save_student.assert_not_called()
 
 
 class TestLiveTestResults(TestCase):
@@ -76,3 +138,20 @@ class TestLiveTestResults(TestCase):
 		self.assertIn(b'0.90', response.data)
 		# assert avg score
 		self.assertIn(b'0.8', response.data)
+
+
+class TestHelpers(TestCase):
+
+	def test_average_score(self):
+		"""
+		Tests that given a dictionary where the value is a float,
+		it returns the correct average of the float values.
+		"""
+		test_results = {'studentId12345': '0.90', 'studentId56789': '0.70'}
+		avg_score = average_score(test_results)
+		self.assertEqual(avg_score, 0.8)
+
+	def test_average_score_empty_dict(self):
+		"""Tests that when a dictionary is empty, average score returned is 0."""
+		avg_score = average_score({})
+		self.assertEqual(avg_score, 0)
